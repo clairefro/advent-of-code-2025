66ca82abafe82c07774a2760800e28792c537a39
Claire Froelich <claire.froelich@gmail.com>
2025-12-02 08:55:43 -0500

    rm comment

--- util/load.js
+++ util/load.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 
-/** For loading inputs from file to string */
 const load = (_path) => {
   return fs.readFileSync(_path, "utf8", function (err, data) {
     if (err) throw err;
